adcc83f3547748884d367ad669202359526a0836
Leon Suarez <leonsua@gmail.com>
2022-02-16 15:06:43 -0500

    create end point to get movie by id

--- movies-demo-backend/routes/movies.js
+++ movies-demo-backend/routes/movies.js
@@ -16,4 +16,18 @@ router.get('/movies', async (req, res, next) => {
   }
 });
 
+router.get('/movies/:_id', async (req, res, next) => {
+  try {
+    const movieId = req.params._id;
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = await client.db('moviesDataBase');
+
+    const query = {_id: ObjectId(movieId)}
+    const movie = await db.collection('movies').findOne(query);
+    res.status(200).json({ message: 'One Movie', movie });
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+});
+
 module.exports = router;
