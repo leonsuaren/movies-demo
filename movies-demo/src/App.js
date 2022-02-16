9cd104c57b31fdcea4bdfbd2f7905258014ae760
Leon Suarez <leonsua@gmail.com>
2022-02-15 21:32:03 -0500

    added not found view

--- movies-demo/src/App.js
+++ movies-demo/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import { Navbar } from '../src/components/navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Navbar } from '../src/components/navbar';
+import { Home } from '../src/views/home';
+import { Favorites } from '../src/views/favorites';
+import { NowPlaying } from '../src/views/now-playing';
+import { NotFound } from '../src/views/not-found';
 
 function App() {
   return (
     <div>
         <Navbar />
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='now-playing' element={<NowPlaying />} />
+            <Route path='favorites' element={<Favorites />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
