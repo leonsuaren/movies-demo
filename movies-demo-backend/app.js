d8244d4d79c2e55cb389826c89880695a38b98a4
Leon Suarez <leonsua@gmail.com>
2022-02-16 09:58:31 -0500

    added cors to the backend and use it on app.js

--- movies-demo-backend/app.js
+++ movies-demo-backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const indexRouter = require('./routes/index');
+const cors = require('cors');
 
 const port = 8080;
 
 const app = express();
 
 app.use('/', indexRouter);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
