4d273d024ffd150f08256b13d079472e4553a8f9
Leon Suarez <leonsua@gmail.com>
2022-02-17 20:53:52 -0500

    created favorites router

--- movies-demo-backend/routes/movies.js
+++ movies-demo-backend/routes/movies.js
@@ -4,7 +4,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 
 router.get('/movies', async (req, res, next) => {
-  try{
+  try {
     const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
     const db = await client.db('moviesDataBase');
 
@@ -22,12 +22,26 @@ router.get('/movies/:_id', async (req, res, next) => {
     const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
     const db = await client.db('moviesDataBase');
 
-    const query = {_id: ObjectId(movieId)}
+    const query = { _id: ObjectId(movieId) }
     const movie = await db.collection('movies').findOne(query);
     res.status(200).json({ message: 'One Movie', movie });
   } catch (error) {
     res.status(404).json({ error: error });
   }
 });
+
+router.get('/favorites', async (req, res, next) => {
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = await client.db('moviesDataBase');
+
+    const query = { favorite: true };
+    const favoriteMovie = await db.collection('movies').find(query).toArray();
+    res.status(200).json({ message: 'Favorites', favoriteMovie });
+
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+})
 
 module.exports = router;
